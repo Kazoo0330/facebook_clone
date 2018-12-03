ab4485378259235e393137d1382e8f1051a5d6f1
legopo <legopo@legoponoMacBook-ea.local>
2018-12-03 13:51:12 +0900

    ログアウト機能の追加とログイン時とログアウト時で表示の分岐させる仕様の追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:sessions][:email].downcase)
-    if user && user.authenticate(params[:sessions][:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'Login failed'
       render 'new'
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'Logout'
+    redirect_to new_session_path
   end
 
 end
